a79397b178f67de6488997a97aa31198ae2fda02
Luminqi <424450414@qq.com>
2018-12-28 17:52:58 +0800

    initial commit

---
+++ autocomplete/src/App.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import AutoComplete from './autoComplete/AutoComplete'
+import AutoComplete_test from './autoComplete/AutoComplete_test' 
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AutoComplete />
+        <AutoComplete_test />
+      </div>
+    );
+  }
+}
+
+export default App;
